bbed336664abb1cea719a57e5b3605435aa51d58
Musty260 <93217594+musty-xyz@users.noreply.github.com>
2021-11-11 07:45:48 +0000

    Made mint site kinda spiffy

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Countdown from "react-countdown";
 import { Button, CircularProgress, Snackbar } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
@@ -26,6 +26,69 @@ const CounterText = styled.span``; // add your styles here
 const MintContainer = styled.div``; // add your styles here
 
 const MintButton = styled(Button)``; // add your styles here
+
+const float = keyframes`
+    0% {
+        transform: translatey(0px);
+    }
+    50% {
+        transform: translatey(-20px);
+    }
+    100% {
+        transform: translatey(0px);
+    }           
+`;
+
+const fadeInOut = keyframes`
+    0% {
+        opacity: 1;
+    }
+    9.52% {
+        opacity: 1;
+    }
+    14.29% {
+        opacity: 0;
+    }
+    95.24% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+`;
+
+const ImgContainer = styled.div`
+    height: 420px;
+    width: 420px;
+    position: relative;
+    margin: 0 auto;
+    animation: ${float} 5s ease-in-out infinite;
+
+    @media (max-width: 1100px) {
+        display: none;
+    }
+`; 
+
+const ImgWrap = styled.img`
+    height: 420px;
+    width: 420px;
+    position: absolute;
+    left: 0;
+    animation: ${fadeInOut} 21s ease-in-out infinite;
+`;
+
+const DiscordLink = styled.img`
+    height: 50px;
+    filter: invert(1);
+    position: absolute;
+    left: 50px;
+    bottom: 25px;
+    transition: all 0.15s ease-in-out;
+    opacity: 50%;
+    &:hover {
+        opacity: 100%;
+    }
+`;
 
 export interface HomeProps {
     candyMachineId: anchor.web3.PublicKey;
@@ -201,46 +264,83 @@ const Home = (props: HomeProps) => {
 
     return (
         <main>
-            {wallet && (
-                <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
-            )}
 
-            {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
+            <div style={{ fontFamily: "Gideon Roman", fontSize: "60px", margin: "25px" }}>
+                0euvre AI
+            </div>
 
-            {wallet && <p>Total Available: {itemsAvailable}</p>}
+            <div style={{ display:"flex" }}>
 
-            {wallet && <p>Redeemed: {itemsRedeemed}</p>}
+                <ImgContainer>
+                    <ImgWrap src="/preview_assets/1.png" alt="Preview 1" style={{ animationDelay: "18s" }}/>
+                    <ImgWrap src="/preview_assets/2.png" alt="Preview 1" style={{ animationDelay: "15s" }}/>
+                    <ImgWrap src="/preview_assets/3.png" alt="Preview 1" style={{ animationDelay: "12s" }}/>
+                    <ImgWrap src="/preview_assets/4.png" alt="Preview 1" style={{ animationDelay: "9s" }}/>
+                    <ImgWrap src="/preview_assets/5.png" alt="Preview 1" style={{ animationDelay: "6s" }}/>
+                    <ImgWrap src="/preview_assets/6.png" alt="Preview 1" style={{ animationDelay: "3s" }}/>
+                    <ImgWrap src="/preview_assets/7.png" alt="Preview 1" style={{ animationDelay: "0s" }}/>
+                </ImgContainer>
 
-            {wallet && <p>Remaining: {itemsRemaining}</p>}
+                <div style={{ marginTop:"20vh", fontFamily: "Lato", fontSize: "18px" }}>
+                    {wallet && (
+                        <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
+                    )}
 
-            <MintContainer>
-                {!wallet ? (
-                    <ConnectButton>Connect Wallet</ConnectButton>
-                ) : (
-                    <MintButton
-                        disabled={!isWhitelisted || isSoldOut || isMinting || !isActive} //change happened here
-                        onClick={onMint}
-                        variant="contained"
-                    >
-                        {isSoldOut ? (
-                            "SOLD OUT"
-                        ) : isActive ? (
-                            isMinting ? (
-                                <CircularProgress />
-                            ) : (
-                                "MINT"
-                            )
+                    {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
+
+                    {/** 
+                    {wallet && <p>Supply: {itemsAvailable}</p>}
+
+                    {wallet && <p>Redeemed: {itemsRedeemed}</p>}
+                    */}
+
+                    {wallet && <p>Remaining: {itemsRemaining} / {itemsAvailable}</p>}
+
+                    <MintContainer>
+                        {!wallet ? (
+                            <ConnectButton>Connect Wallet</ConnectButton>
                         ) : (
-                            <Countdown
-                                date={startDate}
-                                onMount={({ completed }) => completed && setIsActive(true)}
-                                onComplete={() => setIsActive(true)}
-                                renderer={renderCounter}
-                            />
+                            <MintButton
+                                disabled={!isWhitelisted || isSoldOut || isMinting || !isActive} //change happened here
+                                onClick={onMint}
+                                variant="contained"
+                            >
+                                {isSoldOut ? (
+                                    "SOLD OUT"
+                                ) : isActive ? (
+                                    isMinting ? (
+                                        <CircularProgress />
+                                    ) : (
+                                        "MINT"
+                                    )
+                                ) : (
+                                    <Countdown
+                                        date={startDate}
+                                        onMount={({ completed }) => completed && setIsActive(true)}
+                                        onComplete={() => setIsActive(true)}
+                                        renderer={renderCounter}
+                                    />
+                                )}
+                            </MintButton>
                         )}
-                    </MintButton>
-                )}
-            </MintContainer>
+                    </MintContainer>
+                </div>
+
+                <ImgContainer style={{ marginTop: "300px", left: "30px" }}>
+                    <ImgWrap src="/preview_assets/8.png" alt="Preview 1" style={{ animationDelay: "18s" }}/>
+                    <ImgWrap src="/preview_assets/9.png" alt="Preview 1" style={{ animationDelay: "15s" }}/>
+                    <ImgWrap src="/preview_assets/10.png" alt="Preview 1" style={{ animationDelay: "12s" }}/>
+                    <ImgWrap src="/preview_assets/11.png" alt="Preview 1" style={{ animationDelay: "9s" }}/>
+                    <ImgWrap src="/preview_assets/12.png" alt="Preview 1" style={{ animationDelay: "6s" }}/>
+                    <ImgWrap src="/preview_assets/13.png" alt="Preview 1" style={{ animationDelay: "3s" }}/>
+                    <ImgWrap src="/preview_assets/14.png" alt="Preview 1" style={{ animationDelay: "0s" }}/>
+                </ImgContainer>
+
+            </div>
+
+            <a href="https://discord.com/invite/qMbBVNASTS">
+                <DiscordLink src="/discord-brands.svg" alt="Discord Link"/>
+            </a>
 
             <Snackbar
                 open={alertState.open}
